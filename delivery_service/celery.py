cc920b34b92a3a50b2bce79fbd9a11e29907afc9
Alexandr3008 <soloshenko3008@gmail.com>
2025-05-27 19:19:04 +0300

    Delete delivery_service directory

--- delivery_service/celery.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delivery_service.settings')
-app = Celery('delivery_service')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
