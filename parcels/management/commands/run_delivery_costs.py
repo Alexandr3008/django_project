581570bb95d8fb9ae0d6ead876e67b28a50e0ed2
Alexandr3008 <soloshenko3008@gmail.com>
2025-05-27 19:19:19 +0300

    Delete parcels directory

--- parcels/management/commands/run_delivery_costs.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from parcels.tasks import calculate_delivery_costs
-
-class Command(BaseCommand):
-    help = 'Ручной запуск расчета стоимости доставки'
-
-    def handle(self, *args, **kwargs):
-        calculate_delivery_costs()
-        self.stdout.write(self.style.SUCCESS('Стоимость доставки рассчитана вручную'))
